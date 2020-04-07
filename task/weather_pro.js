3c4582ab038d48c30a2d8bcd8b9b449b26be3f4e
之都十渡 <52275745+hudongxin@users.noreply.github.com>
2020-04-08 00:20:10 +0800

    Update weather_pro.js

--- task/weather_pro.js
+++ task/weather_pro.js
@@ -134,10 +134,10 @@ if (isSurge) {
 https://github.com/sazs34/TaskConfig/blob/master/assets/weather_pro.md
  */
 let config = {
-    darksky_api: "填这里", //从https://darksky.net/dev/ 上申请key填入即可
-    aqicn_api: "填这里", //从http://aqicn.org/data-platform/token/#/ 上申请key填入即可
-    huweather_apiKey: "填这里", //和风天气APIkey,可自行前往 https://dev.heweather.com/ 进行获取
-    lat_lon: "填这里", //请填写经纬度,直接从谷歌地图中获取即可
+    darksky_api: "440c5d2e99f341447a15a615492f3fa7", //从https://darksky.net/dev/ 上申请key填入即可
+    aqicn_api: "5dc12dc6b3459cb49fc9079a5d0f4ebd481565c5", //从http://aqicn.org/data-platform/token/#/ 上申请key填入即可
+    huweather_apiKey: "49342ccf37734c6e8b40b1a0082e18a2", //和风天气APIkey,可自行前往 https://dev.heweather.com/ 进行获取
+    lat_lon: "30.011111111111113, 120.21888888888888", //请填写经纬度,直接从谷歌地图中获取即可
     lang: "zh", //语言,随意切换为您想要的语言哦(zh/zh-tw/ja/en/fr/...)
     log: 0, //调试日志,0为不开启,1为开启,2为开启精简日志
     timeout: 0, //超时时间,单位毫秒(1000毫秒=1秒),一般不推荐修改[为0则不限制超时时间]
